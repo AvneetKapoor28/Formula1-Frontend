4e8345377bf9c778491e26fd260a3d3d1b267def
AvneetKapoor28 <avneetsingh.kapoor2022@vitstudent.ac.in>
2024-10-26 16:47:53 +0530

    Refactor API call in PopupDriverStandingsTable component

--- src/app/Components/RacePopUp/PopupDriverStandings/PopupDriverStandingsTable.jsx
+++ src/app/Components/RacePopUp/PopupDriverStandings/PopupDriverStandingsTable.jsx
@@ -6,24 +6,7 @@ import { PastSeasonsPageContext } from "@/app/Context/PastSeasonsPageProvider";
 
 const PopupDriverStandingsTable = (props) => {
     const { selectedRound, selectedYear } = useContext(PastSeasonsPageContext);
-  const [roundDriverStandingsList, setRoundDriverStandingsList] = useState([]); // Driver standings state
-  const [loading, setLoading] = useState(true); // Loading state
 
-  useEffect(() => {
-    setLoading(true); // Start loading
-    axios
-      .get(`${process.env.NEXT_PUBLIC_API_URL}/pastData/driverStandings/${selectedYear}/${selectedRound}`)
-      .then((response) => {
-        setRoundDriverStandingsList(response.data); // Update roundDataStandingsList
-        setLoading(false); // Stop loading
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-        setRoundDriverStandingsList([]); // Clear roundDataStandingsList on error
-        setLoading(false); // Stop loading on error
-      });
-  }, [selectedRound, selectedYear]);
   return (
     // INSERT LOADING LOGIC
     <div>PopupDriverStandingsTable</div>
--- src/app/Components/RacePopUp/RacePopUp.jsx
+++ src/app/Components/RacePopUp/RacePopUp.jsx
@@ -2,27 +2,62 @@ import React, { useState, useContext, useEffect } from "react";
 import "./RacePopUp.css";
 import { PastSeasonsPageContext } from "@/app/Context/PastSeasonsPageProvider";
 import PopupDriverStandingsTable from "./PopupDriverStandings/PopupDriverStandingsTable";
+import axios from "axios";
+import loading_animation from "../../../Assets/loading_animation.webm";
 
 const RacePopUp = ({ setIsPopupOpen }) => {
   const [showPopup, setShowPopup] = useState(false);
-  const { selectedRound } = useContext(PastSeasonsPageContext);
-  
+  const { selectedRound, selectedYear } = useContext(PastSeasonsPageContext);
+  const [roundData, setroundData] = useState([]); // Driver standings state
+  const [loadingPopup, setLoadingPopup] = useState(true); // LoadingPopup state
+
   useEffect(() => {
+    setLoadingPopup(true); // Start loadingPopup
     setShowPopup(true);
-  }, [selectedRound]);
-  
+    axios
+      .get(
+        `${process.env.NEXT_PUBLIC_API_URL}/pastData/driverStandings/${selectedYear}/${selectedRound}`
+      )
+      .then((response) => {
+        setroundData(response.data[0]); // Update roundDataStandingsList
+        setLoadingPopup(false); // Stop loadingPopup
+        console.log(response.data[0]);
+      })
+      .catch((error) => {
+        console.log(error);
+        setroundData([]); // Clear roundDataStandingsList on error
+        setLoadingPopup(false); // Stop loadingPopup on error
+      });
+  }, [selectedRound, selectedYear]);
+
   const handleClose = () => {
     setShowPopup(false);
     setTimeout(() => setIsPopupOpen(false), 400); // Close after transition duration
   };
 
+  if (loadingPopup) {
+    return (
+      <div className={`popup-overlay ${showPopup ? "show" : ""}`}>
+        <div className={`popup-window ${showPopup ? "show" : ""}`}>
+          <button className="close-button" onClick={handleClose}>
+            x
+          </button>
+          <div className="loading-container">
+            <video autoPlay loop muted className="video-player">
+              <source src={loading_animation} type="video/webm" />
+            </video>
+          </div>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className={`popup-overlay ${showPopup ? "show" : ""}`}>
       <div className={`popup-window ${showPopup ? "show" : ""}`}>
         <button className="close-button" onClick={handleClose}>
           x
         </button>
-        <div className="racename main-heading">RACE NAME</div>
+        <div className="racename main-heading">{roundData.raceName}</div>
         <div className="raceround">{selectedRound}</div>
         <PopupDriverStandingsTable />
       </div>
