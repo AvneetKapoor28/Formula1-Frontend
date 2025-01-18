539c3902772ae3454060dc8257eef9620c8998d8
AvneetKapoor28 <avneetsingh.kapoor2022@vitstudent.ac.in>
2025-01-19 01:00:28 +0530

    roughly made RaceDetails

--- src/app/Components/RaceDetails/RaceDetails.jsx
+++ src/app/Components/RaceDetails/RaceDetails.jsx
@@ -1,6 +1,11 @@
 import React, { useState, useContext, useEffect } from "react";
 import { PastSeasonsPageContext } from "@/app/Context/PastSeasonsPageProvider";
 import axios from "axios";
+import "./RaceDetails.css";
+import RaceStandingsItem from "../RaceStandingsItem/RaceStandingsItem";
+import loading_animation from "../../../Assets/loading_animation.webm";
+import RaceDetailsHeader from "./RaceDetailsHeader";
+
 const RaceDetails = () => {
   const [roundData, setroundData] = useState([]); // Driver standings state
   const {
@@ -9,30 +14,72 @@ const RaceDetails = () => {
     selectedRound,
     selectedYear,
   } = useContext(PastSeasonsPageContext);
+  const [loading, setLoading] = useState(true); // Loading state
+
+  const formatTime = (millis) => {
+    const hours = Math.floor(millis / (1000 * 60 * 60));
+    const minutes = Math.floor((millis % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((millis % (1000 * 60)) / 1000);
+    const milliseconds = millis % 1000;
+
+    // Format with leading zeros
+    const formattedHours = hours.toString().padStart(2, "0");
+    const formattedMinutes = minutes.toString().padStart(2, "0");
+    const formattedSeconds = seconds.toString().padStart(2, "0");
+    const formattedMilliseconds = milliseconds.toString().padStart(3, "0");
+
+    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}.${formattedMilliseconds}`;
+  };
 
   useEffect(() => {
+    setLoading(true); // Start loading
     axios
       .get(
         `${process.env.NEXT_PUBLIC_API_URL}/pastData/driverStandings/${selectedYear}/${selectedRound}`
       )
       .then((response) => {
         setroundData(response.data[0]); // Update roundDataStandingsList
-        console.log(response.data[0]);
+        console.log(response.data[0].Results);
+        setLoading(false); // Stop loading
       })
       .catch((error) => {
         console.log(error);
+        setLoading(false); // Stop loading on error
         setroundData([]); // Clear roundDataStandingsList on error
       });
   }, [selectedRound, selectedYear]);
+
+  if (loading) {
+    return (
+      <div className="loading-container">
+        <video autoPlay loop muted className="video-player">
+          <source src={loading_animation} type="video/webm" />
+        </video>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <div>
-        <div className="selectedLeftSidebar">
-          <div className="selectedRaceName">ABCD GRAND PRIX</div>
-          
-        </div>
-        <div className="selectedRaceDate">10 Jul - 13 Jul</div>
-        <div className="selectedRacelocation">Bahrain, AbuDhabi</div>
+    <div className="race-standings-main-container">
+      <div className="race-standings-container">
+        <RaceDetailsHeader />
+        {roundData.Results?.map((result, index) => (
+          <RaceStandingsItem
+            key={index}
+            position={result.position}
+            driverNumber={result.Driver.permanentNumber}
+            driverName={`${result.Driver.givenName} ${result.Driver.familyName}`}
+            points={result.points}
+            status={result.status}
+            time={
+              result.status === "Finished"
+                ? index === 0
+                  ? formatTime(result.Time.millis)
+                  : result.Time.time
+                : result.status
+            }
+          />
+        ))}
       </div>
     </div>
   );
---
+++ src/app/Components/RaceDetails/RaceDetailsHeader.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const RaceDetailsHeader = () => {
+  return (
+    <div className="race-details-header">
+      <div className="race-details-header-position">Position</div>
+      <div className="race-details-header-number">Number</div>
+      <div className="race-details-header-drivername">Driver</div>
+      <div className="race-details-header-points">Points</div>
+      <div className="race-details-header-time">Time</div>
+    </div>
+  );
+};
+
+export default RaceDetailsHeader;
