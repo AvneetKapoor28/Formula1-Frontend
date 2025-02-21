ab5e77fb04d2a967f0dcfa46ba7d60a59fc398b4
AvneetKapoor28 <avneetsingh.kapoor2022@vitstudent.ac.in>
2025-02-21 12:34:51 +0530

    Add layout and styling for RaceAnalytics component; implement container and elements for race data display

--- src/app/Components/RaceAnalytics/RaceAnalytics.jsx
+++ src/app/Components/RaceAnalytics/RaceAnalytics.jsx
@@ -3,7 +3,13 @@ import './RaceAnalytics.css'
 
 const RaceAnalytics = () => {
   return (
-    <div>RaceAnalytics</div>
+    <div className='raceanalytics-container'>
+        <div className='raceanalytics-element'>Fastest Lap Gear Shifts</div>
+        <div className='raceanalytics-element'>Team Pace Comparison</div>
+        <div className='raceanalytics-element'>Tyre Strategies</div>
+        <div className='raceanalytics-element'>Driver Laptimes Distribution</div>
+        <div className='raceanalytics-element'>Position Changes During Race</div>
+    </div>
   )
 }
 
